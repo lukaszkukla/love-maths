a57267e076e8a601292460f9113e5cbea6da243f
lukaszkukla <lukaszkukla@gmail.com>
2022-02-17 17:49:43 +0000

    feat: display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('you clicked submit');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition');
 })
 
 /**
  * the main game 'loop', called when the script is first loaded
  * and afte the user's answer has beeen processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 ) + 1;
     let num2 = Math.floor(Math.random() * 25 ) + 1;
+
+    if(gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -43,8 +52,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
